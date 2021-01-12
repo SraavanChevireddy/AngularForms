46130c8b5be238ac0e704c945bf2cd5346b0c55e
Sraavan Chevireddy <schevireddy@saventech.com>
2021-01-12 22:59:18 +0530

    Form validations added

--- src/app/user-setttings-form/user-setttings-form.component.ts
+++ src/app/user-setttings-form/user-setttings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-setttings-form',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-setttings-form.component.css']
 })
 export class UserSetttingsFormComponent implements OnInit {
+
+  userSettings : UserSettings = {
+    name : 'Sraavan Chevireddy',
+    emailOffers : false,
+    interfaceStyle : 'dark',
+    notes : 'This is sample notes',
+    subscription : 'Annual'
+  }
+  
+  copyWithSpreadOperator = { ...this.userSettings }
+  copyWithoutSpreadOperator = this.userSettings
+
 
   constructor() { }
 
